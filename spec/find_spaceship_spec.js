8b0bb4619d5041e575143ef67c800e09b8566b5e
BossAleks123 <Aleksandartesinic720@gmail.com>
2026-05-18 20:26:15 +0200

    SECOND TEST CASE - NO SPACESHIP - TEST FAILED

--- spec/find_spaceship_spec.js
+++ spec/find_spaceship_spec.js
@@ -7,4 +7,9 @@ describe('Find SpaceShip', function() {
 		let MAP = "";
 		expect(FindSpaceShip.add(MAP)).toEqual("NO SPACE!");
 	});
+
+	it('should return "" for an empty string', function() {
+		let MAP = ".....";
+		expect(FindSpaceShip.add(MAP)).toEqual("Spaceship lost forever");
+	});
 });
