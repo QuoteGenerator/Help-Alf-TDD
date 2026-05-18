db3d4f7febfdbdbd1f054c51435899a8d756c2e4
BossAleks123 <Aleksandartesinic720@gmail.com>
2026-05-18 20:34:40 +0200

    SECOND TEST CASE - NO SPACESHIP - TEST SUCCESSFULL. + error fix in Spec: variable not same name anymore like class name

--- spec/find_spaceship_spec.js
+++ spec/find_spaceship_spec.js
@@ -1,15 +1,15 @@
 describe('Find SpaceShip', function() {
 	beforeEach(function() {
-		FindSpaceShip = new FindSpaceShip();
+		spaceShipFinder = new FindSpaceShip();
 	});
 
 	it('should return "NO SPACE" for an empty string', function() {
 		let MAP = "";
-		expect(FindSpaceShip.add(MAP)).toEqual("NO SPACE!");
+		expect(spaceShipFinder.add(MAP)).toEqual("NO SPACE!");
 	});
 
 	it('should return "" for an empty string', function() {
 		let MAP = ".....";
-		expect(FindSpaceShip.add(MAP)).toEqual("Spaceship lost forever");
+		expect(spaceShipFinder.add(MAP)).toEqual("Spaceship lost forever");
 	});
 });
--- src/find_spaceship.js
+++ src/find_spaceship.js
@@ -1,5 +1,6 @@
 // StringCalculator.js
 function FindSpaceShip() {}
 FindSpaceShip.prototype.add = function(galaxyMap) {
-	return "NO SPACE!";
+
+	return galaxyMap == "" ? "NO SPACE!" : "Spaceship lost forever";
 };
