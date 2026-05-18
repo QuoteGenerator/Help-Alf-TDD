a26fe1d8a8925f53103797d0314f76feb6cf395f
BossAleks123 <Aleksandartesinic720@gmail.com>
2026-05-18 20:40:34 +0200

    TEST CASE 3 - ONE LINE MAP - TEST FAILED

--- spec/find_spaceship_spec.js
+++ spec/find_spaceship_spec.js
@@ -13,5 +13,9 @@ describe('Find SpaceShip', function() {
 		expect(spaceShipFinder.add(MAP)).toEqual("Spaceship lost forever");
 	});
 
+	it('should return Coordinates "[2,0]" for a one line Map-string with 1 X in the Map', function() {
+		let MAP = "..X..";
+		expect(spaceShipFinder.add(MAP)).toEqual([2,0]);
+	});
 
 });
