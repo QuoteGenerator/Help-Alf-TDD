341a85a6a2e214be5566e8cb556031f01fffbd0a
BossAleks123 <Aleksandartesinic720@gmail.com>
2026-05-18 20:48:24 +0200

    TEST CASE 3 - ONE LINE MAP - TEST SUCCESSFUL

--- src/find_spaceship.js
+++ src/find_spaceship.js
@@ -2,5 +2,14 @@
 function FindSpaceShip() {}
 FindSpaceShip.prototype.add = function(galaxyMap) {
 
+	const mapArray = galaxyMap.split("");
+	console.log(mapArray);
+
+	for(let i = 0; i < mapArray.length; i++){
+		if(mapArray[i] == 'X'){
+			return [i, 0];
+		}
+	}
+
 	return galaxyMap == "" ? "NO SPACE!" : "Spaceship lost forever";
 };
